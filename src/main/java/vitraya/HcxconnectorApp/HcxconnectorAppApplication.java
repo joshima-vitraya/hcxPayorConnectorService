b622c2fc1016c2783e88c47653e63432f1b6a3d6
joshima-vitraya <joshima.antonnavis@vitrayatech.com>
2022-10-11 14:05:38 +0530

    controller changes

--- src/main/java/vitraya/HcxconnectorApp/HcxconnectorAppApplication.java
+++ src/main/java/vitraya/HcxconnectorApp/HcxconnectorAppApplication.java
@@ -2,14 +2,22 @@ package vitraya.HcxconnectorApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@RestController
 public class HcxconnectorAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HcxconnectorAppApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	public String displayHello(){
+		String welcomeText="Hello from Payor Connector";
+		return welcomeText;
+	}
 
 }
